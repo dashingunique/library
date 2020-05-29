816ff94f29a60acdeda1a46369cc362e95a37cc9
李奇林 <1107842285@qq.com>
2020-05-29 16:42:06 +0800

    thinkphp6 extension library

---
+++ src/library/Helper.php
@@ -0,0 +1,14 @@
+<?php
+
+use library\Collection;
+
+if (!function_exists('uniqueCollection')) {
+    /**
+     * 从给定的值创建一个集合
+     * @param  null  $value
+     * @return Collection
+     */
+    function uniqueCollection($value = null) {
+        return new Collection($value);
+    }
+}
