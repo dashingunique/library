d8776fbb47b518149fbe2be9484bb87057864fca
张大宝的程序人生 <1107842285@qq.com>
2020-07-28 16:01:23 +0800

    新增获取类名方法

--- src/exceptions/AuthException.php
+++ src/exceptions/AuthException.php
@@ -4,7 +4,7 @@
  * ==========================================================================
  * @link      http://erp.chaolizi.cn/
  * @license   http://erp.chaolizi.cn/license.html License
- * @Desc      参数缺失异常
+ * @Desc      授权信息验证错误
  * ==========================================================================
  * @author    张大宝的程序人生 <1107842285@qq.com>
  */
@@ -12,8 +12,7 @@ declare(strict_types=1);
 
 namespace dashingUnique\library\exceptions;
 
-
-class InvalidArgumentException extends Exception
+class AuthException extends Exception
 {
 
 }
